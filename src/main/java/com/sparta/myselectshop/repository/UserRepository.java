4a46b0da22d95f992176761c56cd9783851f1d38
UMK <haga5032@naver.com>
2023-04-24 17:25:20 +0900

    관심상품 기능 구현 완료

--- src/main/java/com/sparta/myselectshop/repository/UserRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.sparta.myselectshop.repository;
 import com.sparta.myselectshop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
